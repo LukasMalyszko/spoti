839281cd46b8648f8e5374b7d87d60056eb57a6c
Lukasz Malyszko <lukas.malyszko@gmail.com>
2023-04-12 16:56:54 +0200

    dodanie nowych func

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -1,3 +1,19 @@
 
 import '../styles/home-page.scss'
 
+const dots = document.querySelectorAll(".dots");
+const songElement = document.querySelector(".playlist-component__song-element");
+
+function showList() {
+    const ul = document.createElement("ul");
+    ul.classList.add("playlist-component__showing-list");
+    const li1 = document.createElement("li");
+    ul.appendChild(li1);
+    li1.innerHTML = "Add to queue";
+    
+    songElement.appendChild(ul);
+}
+
+for(const dot of dots){
+    dot.addEventListener("click", showList);
+}
